79d68d1e004548aa52f1a38ec42b24e94c38a396
cseazeem <azeema224143@gmail.com>
2025-01-14 19:43:10 +0530

    un

--- perfume-store/src/main/java/com/perfume/store/security/SecurityConfig.java
+++ perfume-store/src/main/java/com/perfume/store/security/SecurityConfig.java
@@ -1,14 +1,11 @@
 package com.perfume.store.security;
 
-import com.perfume.store.service.UserInfoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
